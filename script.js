688a7db263a8f1ed1427ed724e707719c4514243
Aishwarya Adhikari <aishwaryaadhikari675@gmail.com>
2025-06-22 17:52:36 +0545

    Final Update

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-const display =document.querySelector('#display')
+const display = document.querySelector('#display')
 
 
 const buttons = document.querySelectorAll('button')
 
-buttons.forEach((item)=>{
-   item.onclick= ()=>{
-    if(item.id=='clear'){
-        display.innerText=''
-   }
-   else if(item.id=='backspace'){
-        let string = display.innerText.toString()
-        display.innerText = string.substring(0,string.length-1)
-   }
-   else if(display.innerText!='' && item.id == 'equals'){
-         display.innerText = eval(display.innerText)
-   }
+buttons.forEach((item) => {
+    item.onclick = () => {
+        if (item.id == 'clear') {
+            display.innerText = ''
+        }
+        else if (item.id == 'backspace') {
+            let string = display.innerText.toString()
+            display.innerText = string.substring(0, string.length - 1)
+        }
+        else if (display.innerText != '' && item.id == 'equals') {
+            display.innerText = eval(display.innerText)
+        }
 
-   else if(display.innerText=='' && item.id == 'equals'){
-            display.innerText = 'Please Enter Something to Calculate'
-            setTimeout(() => (display.innerText = ''), 2000)
-   }
-    else{
-          display.innerText += item.id
-     }
+        else if (display.innerText == '' && item.id == 'equals') {
+            display.style.justifyContent = 'flex-start'
+            display.innerText = 'Empty!'
+            setTimeout(() => {
+                display.innerText = '';
+                display.style.justifyContent = 'flex-end'
+            }, 2000)
+        }
+        else {
+            display.innerText += item.id
+        }
 
 
-}
+    }
 })
